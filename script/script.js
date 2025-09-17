f047379fb3bed45549acb3f9abba2f56718bc5b2
Sabbir Hossain <sm.sabbir9671@gmail.com>
2025-09-18 00:50:07 +0600

    added date & time

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
 console.log(document);
 
-function getElementById(id){
-    const element = document.getElementById(id);
-    return element;
+function getElementById(id) {
+  const element = document.getElementById(id);
+  return element;
 }
 //event add korar system
 
@@ -11,34 +11,39 @@ function getElementById(id){
 //ফাংশন লেখতে হবে।
 
 //Event delegation
-getElementById('service-box').addEventListener('click',function(e){
-   if(e.target.className.includes('btn-call')){
+getElementById("service-box").addEventListener("click", function (e) {
+  if (e.target.className.includes("btn-call")) {
     // alert('card-clicked')
     const serviceButton = e.target;
 
-    const serviceTitle = serviceButton.parentNode.parentNode.children[1].children[0].innerText;
-    const serviceCall = serviceButton.parentNode.parentNode.children[2].children[0].innerText;
-    const serviceDate = "";
-    // console.log(serviceCall);
+    const serviceTitle =
+      serviceButton.parentNode.parentNode.children[1].children[0].innerText;
+    const serviceCall =
+      serviceButton.parentNode.parentNode.children[2].children[0].innerText;
+    const time = {
+      date: new Date().toLocaleTimeString(),
+    };
+    // console.log(time);
 
-    const newHistoryContainer = getElementById('service-container');
-    const newHistory = document.createElement('div');
+    const newHistoryContainer = getElementById("service-container");
+    const newHistory = document.createElement("div");
     newHistory.innerHTML = `
-                    <div class="bg-white shadow-lg rounded-xl p-2 flex gap-6">
+                    <div class="bg-white shadow-lg rounded-xl p-2 flex gap-4">
                         <div>
-                            <h1 class="font-semibold text-[16px]">${serviceTitle}</h1>
-                            <h1 class="font-semibold text-gray-500 text-[18px] mt-1">${serviceCall}</h1>
+                            <h1 class="font-semibold text-[14px]">${serviceTitle}</h1>
+                            <h1 class="font-semibold text-gray-500 text-[14px] mt-1">${serviceCall}</h1>
                         </div>
-                        <p class="font-[400px]">11:36:58 AM</p>
+                       <div class="timeOFCall">
+               <p class="time text-[#111111] text-[14px] hind-madurai-font">${time.date}</p>
+             </div>
                     </div>
+             
     `;
     newHistoryContainer.append(newHistory);
-   }
-})
-
-document.getElementById('btn-clear').addEventListener('click', function(){
-  const cardContainer = getElementById('service-container');
-  cardContainer.innerHTML ='';
-
+  }
 });
 
+document.getElementById("btn-clear").addEventListener("click", function () {
+  const cardContainer = getElementById("service-container");
+  cardContainer.innerHTML = "";
+});
